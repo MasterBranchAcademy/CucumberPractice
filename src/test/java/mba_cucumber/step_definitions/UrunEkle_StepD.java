b411e7bdef9c2a33b4cd7af11fd7b04d2f7e0f30
gulay <gulaysengulderen@gmail.com>
2023-11-01 19:24:10 +0100

    UrunEkle_StepD has been modified by QA Gulay

--- src/test/java/mba_cucumber/step_definitions/UrunEkle_StepD.java
+++ src/test/java/mba_cucumber/step_definitions/UrunEkle_StepD.java
@@ -2,17 +2,20 @@ package mba_cucumber.step_definitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import mba_cucumber.pages.GreenKartPage;
 import mba_cucumber.pages.PageObject;
 
 public class UrunEkle_StepD extends PageObject {
 
-
+    GreenKartPage greenKartPage = new GreenKartPage();
 
     @Given("Kullanici {string} sitesine gider")
     public void kullaniciSitesineGider(String arg0) {
+
     }
 
     @When("{string} add to chart butonuna tiklar")
     public void addToChartButonunaTiklar(String arg0) {
+
     }
 }
